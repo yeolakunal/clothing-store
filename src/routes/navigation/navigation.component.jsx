437f086856658af3e722262397f61dc88cda8c58
Kunal Yeola <yeolakunal@gmail.com>
2022-07-03 15:32:04 +0530

    Added Routing

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,23 @@
+import { Link, Outlet } from "react-router-dom";
+import { ReactComponent as CRWNLogo } from "../../assets/crown.svg";
+import "./navigation.styles.scss";
+const Navigation = () => {
+  return (
+    <>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <CRWNLogo className="logo" />
+        </Link>
+
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </>
+  );
+};
+
+export default Navigation;
